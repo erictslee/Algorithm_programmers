4645186a228729e3e562b23479a07ff66c40a179
erictslee <70851773+erictslee@users.noreply.github.com>
2024-04-25 11:23:39 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if((num1>=0&&num1<=10000)&&(num2>=0&&num2<=10000)){
+            if(num1==num2){
+                answer=1;
+            }else{
+                answer=-1;
+            }
+        }else{
+            System.out.println("Wrong Input");
+        }
+        return answer;
+    }
+}
